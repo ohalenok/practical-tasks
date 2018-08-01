d0e895da2f2c0f3e297de41d7bb8047a04aa0bbb
Olena Halenok <olena.halenok@gmail.com>
2018-08-01 22:57:22 +0300

    JS function with conditional statement to sort three numbers

---
+++ task2/task2_5.js
@@ -0,0 +1,36 @@
+let x= 5;
+let y=-6;
+let z= 10;
+if (x>y && x>z)
+{
+        if (y>z)
+        {
+            console.log(x + ", " + y + ", " +z);
+        }
+        else
+        {
+            console.log(x + ", " + z + ", " +y);
+        }
+}
+else if (y>x && y >z)
+{
+        if (x>z)
+        {
+             console.log(y + ", " + x + ", " +z);
+        }
+        else
+        {
+             console.log(y + ", " + z + ", " +x);
+        }
+}
+else if (z>x && z>y)
+{
+        if (x>y)
+        {
+            console.log(z + ", " + x + ", " +y);
+        }
+        else
+        {
+            console.log(z + ", " + y + ", " +x);
+        }
+}        
